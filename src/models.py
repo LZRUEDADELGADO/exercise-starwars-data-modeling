110ec1de366354b473b1248e80d42af61f28b1f9
Lynda <155568879+LZRUEDADELGADO@users.noreply.github.com>
2025-01-12 17:06:53 +0000

    se agregaron mas clases

--- src/models.py
+++ src/models.py
@@ -16,7 +16,47 @@ class User(Base):
     firstname = Column(String, nullable=True)
     lastname = Column(String, nullable=True)
 
+    followers = relationship('Follower', back_populates='user_to', foreign_keys='Follower.user_to_id')
+    following = relationship('Follower', back_populates='user_from', foreign_keys='Follower.user_from_id')
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='author')
     favorites = relationship('Favorite', back_populates='user')
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+    user_from = relationship('User', back_populates='following', foreign_keys=[user_from_id])
+    user_to = relationship('User', back_populates='followers', foreign_keys=[user_to_id])
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship('User', back_populates='posts')
+    media = relationship('Media', back_populates='post')
+    comments = relationship('Comment', back_populates='post')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    post = relationship('Post', back_populates='media')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    author = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
 
 class Planet(Base):
     __tablename__ = 'planet'
